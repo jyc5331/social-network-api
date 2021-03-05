c34413e9b1d79b637d636bcfd5a7f5a125ca5cfa
jyc5331 <jcosgrovecoding@gmail.com>
2021-03-05 15:03:24 -0500

    most of users.js done

--- README.md
+++ README.md
@@ -1,34 +1,3 @@
 # social-network-api
 
 Initial commit
-
-setup:
-1 folders and files
-2 Express, Mongoose, mongo
-3 db
-4 express calls
-
-USERS
-User
-
-username
-
-String
-Unique
-Required
-Trimmed
-email
-
-String
-Required
-Unique
-Must match a valid email address (look into Mongoose's matching validation)
-thoughts
-
-Array of \_id values referencing the Thought model
-friends
-
-Array of \_id values referencing the User model (self-reference)
-Schema Settings
-
-Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
--- models/Users.js
+++ models/Users.js
@@ -1,18 +1,43 @@
-const { Schema, Types } = require("mongoose");
+const { Schema, model } = require("mongoose");
 const reactionsSchema = require("./Reactions");
 const thoughtsSchema = require("./Thoughts");
+const opts = { toJSON: { virtuals: true }, id: false };
 
 //users
 
-const usersSchema = new Schema({
-  noteId: {
-    type: Types.ObjectId,
-    default: new Types.ObjectId(),
+const usersSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: "Username is Required",
+      unique: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      required: "Email is Required",
+      match: [/.+@.+\..+/],
+    },
+
+    userCreated: {
+      type: Date,
+      default: Date.now,
+    },
+    //HELP Friends: Array of \_id values referencing the User model (self-reference)
+    //reactions: [reactionsSchema],
+    //thoughts: [thoughtsSchema],
   },
-  noteText: {
-    type: String,
-    required: "Note text is Required",
-  },
+  opts
+);
+
+UserSchema.virtual("friendCount").get(function () {
+  return "hello";
+  //retrieves the length of the user's friends array field on query
+  //return this(friends.length)
 });
 
-module.exports = usersSchema;
+const Users = model("Users", usersSchema);
+
+module.exports = Users;
